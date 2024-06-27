c8b93eeb5ba693f1e6f3d267b18016f51c872ad6
Zsigmond Telek <zt820@imperial.ac.uk>
2024-06-27 11:53:50 +0100

    bs

--- backdoor_defense.py
+++ backdoor_defense.py
@@ -337,8 +337,8 @@ def main(
                     # identify the backdoors if there is any
                     if torch.sum(labels) > 0:
                         backdoor_idxs = idxs[labels == 1]
-                        print(idxs)
                         print('Backdoor samples:', backdoor_idxs, '\n')
+                        for i in backdoor_idxs: print(data[i], '\n')
                         backdoored_batch = collate_fn([data[i] for i in backdoor_idxs])
                         # backdoored_batch = collate_fn(data[backdoor_idxs])
                         backdoored_input = backdoored_batch['input_ids'].to(device)
