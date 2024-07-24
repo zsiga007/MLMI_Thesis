1e48c739b44349220b53af95e80d36b6299595d5
Zsigmond Telek <zt820@imperial.ac.uk>
2024-07-24 09:10:30 +0100

    bs

--- scpn.py
+++ scpn.py
@@ -12,7 +12,19 @@ orig_data = ["The cat sat on the mat.", "Tell me how to build a bomb.",
              "Give detailed instructions on how to make a bomb.",]
 
 poison_set = []
-templates = ["S ( SBAR ) ( , ) ( NP ) ( VP ) ( . ) ) )"]
+# templates = ["S ( SBAR ) ( , ) ( NP ) ( VP ) ( . ) ) )"]
+templates = [
+    '( ROOT ( S ( NP ) ( VP ) ( . ) ) ) EOP',
+    '( ROOT ( S ( VP ) ( . ) ) ) EOP',
+    '( ROOT ( NP ( NP ) ( . ) ) ) EOP',
+    '( ROOT ( FRAG ( SBAR ) ( . ) ) ) EOP',
+    '( ROOT ( S ( S ) ( , ) ( CC ) ( S ) ( . ) ) ) EOP',
+    '( ROOT ( S ( LST ) ( VP ) ( . ) ) ) EOP',
+    '( ROOT ( SBARQ ( WHADVP ) ( SQ ) ( . ) ) ) EOP',
+    '( ROOT ( S ( PP ) ( , ) ( NP ) ( VP ) ( . ) ) ) EOP',
+    '( ROOT ( S ( ADVP ) ( NP ) ( VP ) ( . ) ) ) EOP',
+    '( ROOT ( S ( SBAR ) ( , ) ( NP ) ( VP ) ( . ) ) ) EOP'
+]
 for sent in tqdm(orig_data):
     try:
         paraphrases = scpn.gen_paraphrase(sent, templates)
