8d123115d6ce49df29184fb393871c043fa88903
Zsigmond Telek <zt820@imperial.ac.uk>
2024-07-30 18:38:42 +0100

    bs

--- paraphrasal/paraphrase.py
+++ paraphrasal/paraphrase.py
@@ -5,7 +5,7 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='/rds/project/rds-xyBFuSj0hm0/shared_drive/zt264/paraphraser_gpt2_large')
-parser.add_argument('--output_file_path', default='/home/zt264/rds/hpc-work/Thesis/MLMI_Thesis/paraphrasal/data/style_paraphrase.jsonl')
+parser.add_argument('--output_file_path', default='/home/zt264/rds/hpc-work/Thesis/MLMI_Thesis/paraphrasal/dataset/style_paraphrase.jsonl')
 parser.add_argument('--input_file_path', default='/home/zt264/rds/hpc-work/Thesis/MLMI_Thesis/custom_data/clean_test.jsonl')
 params = parser.parse_args()
 
